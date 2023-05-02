0ad8cea516840c79e2e837d5574bb645be2c3e20
DownloadableFox <58178791+DownloadableFox@users.noreply.github.com>
2023-05-02 16:56:37 -0500

    Changed the mod ID to fit the org

---
+++ src/main/java/dev/downloadablefox/contentstyled/ContentStyled.java
@@ -0,0 +1,16 @@
+package com.example.example_mod;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ContentStyled implements ModInitializer {
+	public static final String MOD_ID = "contentstyled";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+	}
+}
